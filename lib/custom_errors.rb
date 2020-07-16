9a7b635ce805a1badf9fe90d1c7071106d4b866e
Olivia Cubela <oliviacubela@gmail.com>
2020-07-16 20:28:28 +0000

    Automatically backed up by Learn

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -9,12 +9,17 @@ class Person
     self.partner = person
     person.partner = self
   end
+  
+  class PartnerError < StandardError
+  end 
 
 end
 
 beyonce = Person.new("Beyonce")
 beyonce.get_married("Jay-Z")
 puts beyonce.name
+
+
 
 
 
